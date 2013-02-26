9e06567c253e7b9b9d88491d406149bde1c4e7c3
thibaut <thibaut@ubuntu.ubuntu-domain>
2013-02-26 19:11:47 +0100

    Debug ccmodel

--- ccmodel.cpp
+++ ccmodel.cpp
@@ -56,35 +56,44 @@ void CCmodel::print(Landscape* land)
 Landscape* CCmodel::_imageToLandscape(Image* img)
 {
 	Landscape* land = new Landscape();
-	for(int debut = 0; debut < img->size();)
+	float debut = 0;
+	while (debut < img->size())
 	{
 		//std::cout<<" debut = "<< debut <<std::endl;
 		if (img->at(debut) != 0)
-		{int fin = debut;
-			do{
+		{
+			float fin = debut;
+			do
+			{
 				fin++;
-			}while(fin < img->size() && img->at(fin) != 0 );
+			} while(fin < img->size() && img->at(fin) != 0 );
 			//std::cout<<" fin = "<< fin <<std::endl;
+
 			LandscapeElem* object = new LandscapeElem(false,
-				((((debut+fin-1)/2) - VIEW_ANGLE / 2)  / 360.) * 2 * PI,
+				((((debut + fin - 1) / 2) - VIEW_ANGLE / 2)  / 360.) * 2 * PI,
 		    	((float)(fin - debut) / 360) * 2 * PI);
 			land->push_back(object);
-			debut=fin;
+			
+			debut = fin;
 		}//fin debut !=0
-	
-		else if (img->at(debut) == 0)
-		{int fin = debut;
-			do{
+		else
+		{
+			float fin = debut;
+			do
+			{
 				fin++;
-			}while(fin < img->size() && img->at(fin) == 0 );
+			}while (fin < img->size() && img->at(fin) == 0 );
 			//std::cout<<" fin = "<< fin <<std::endl;
+
 			LandscapeElem* object = new LandscapeElem(true,
-				((((debut+fin-1)/2) - VIEW_ANGLE / 2) / 360.) * 2 * PI,
+				((((debut + fin - 1) / 2) - VIEW_ANGLE / 2) / 360.) * 2 * PI,
 				((float)(fin - debut) / 360) * 2 * PI);
 			land->push_back(object);
-			debut=fin;
+
+			debut = fin;
 		}//fin if ==0
 	}//fin du for sur image
+
 	//print(land);
 	return land;
 }
